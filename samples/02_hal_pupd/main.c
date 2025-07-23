3fa5f7d8e82a92aa225f4dcbfb63fef563368e33
ragnar-vallhala <ashutoshvishwakarma208@outlook.com>
2025-07-24 03:30:12 +0530

    Fixed PUPD sample

--- samples/02_hal_pupd/main.c
+++ samples/02_hal_pupd/main.c
@@ -8,19 +8,19 @@ void delay(void)
 
 int main(void)
 {
-    hal_gpio_setmode(GPIO_PA05, OUTPUT, 0);
-    hal_gpio_setmode(GPIO_PA06, INPUT, GPIO_PULLDOWN);
-    if (hal_gpio_getmode(GPIO_PA05) != OUTPUT)
+    hal_gpio_setmode(GPIO_PA05, GPIO_OUTPUT, 0);
+    hal_gpio_setmode(GPIO_PA06, GPIO_INPUT, GPIO_PULLDOWN);
+    if (hal_gpio_getmode(GPIO_PA05) != GPIO_OUTPUT)
         while (1)
             ;
-    hal_gpio_digitalwrite(GPIO_PA05, HIGH); // Test LED
+    hal_gpio_digitalwrite(GPIO_PA05, GPIO_HIGH); // Test LED
     delay();
 
     while (1)
     {
         if (hal_gpio_digitalread(GPIO_PA06))
-            hal_gpio_digitalwrite(GPIO_PA05, HIGH);
+            hal_gpio_digitalwrite(GPIO_PA05, GPIO_HIGH);
         else
-            hal_gpio_digitalwrite(GPIO_PA05, LOW);
+            hal_gpio_digitalwrite(GPIO_PA05, GPIO_LOW);
     }
 }

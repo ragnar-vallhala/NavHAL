fc5f1a1479be0ddcb8ff0406f0e075f92b2fb3b1
ragnar-vallhala <ashutoshvishwakarma208@outlook.com>
2025-08-15 17:27:34 +0530

    Fixed the hal_systick sample by changing calculated psc and arr values avoiding overflows

--- samples/05_hal_systick/main.c
+++ samples/05_hal_systick/main.c
@@ -1,66 +1,57 @@
 #define CORTEX_M4
 #include "navhal.h"
 
-void delay() {
-  for (volatile int i = 0; i < 100000; i++)
-    ;
-}
+hal_pll_config_t pll_cfg;
+hal_clock_config_t cfg = {.source = HAL_CLOCK_SOURCE_HSI};
 
-hal_pll_config_t pll_cfg = {.input_src =
-                                HAL_CLOCK_SOURCE_HSE, // external crystal 8 MHz
-                            .pll_m = 8,
-                            .pll_n = 168,
-                            .pll_p = 2,
-                            .pll_q = 7};
-
-hal_clock_config_t cfg = {.source = HAL_CLOCK_SOURCE_PLL};
-
-void print2(void) { uart2_write("Hello World 2\n\r"); }
-void print3(void) { uart2_write("Hello World 3\n\r"); }
-void print4(void) { uart2_write("Hello World 4\n\r"); }
-void print5(void) { uart2_write("Hello World 5\n\r"); }
-void print6(void) { uart2_write("Hello World 6\n\r"); }
-void print7(void) { uart2_write("Hello World 7\n\r"); }
-void print9(void) { uart2_write("Hello World 9\n\r"); }
-void print12(void) { uart2_write("Hello World 12\n\r"); }
+void print2(void) { uart2_write("Hello World 2\n"); }
+void print3(void) { uart2_write("Hello World 3\n"); }
+void print4(void) { uart2_write("Hello World 4\n"); }
+void print5(void) { uart2_write("Hello World 5\n"); }
+void print6(void) { uart2_write("Hello World 6\n"); }
+void print7(void) { uart2_write("Hello World 7\n"); }
+void print9(void) { uart2_write("Hello World 9\n"); }
+void print12(void) { uart2_write("Hello World 12\n"); }
 
 int main() {
   hal_clock_init(&cfg, &pll_cfg);
   systick_init(1000);
   uart2_init(9600);
 
-  timer_init(TIM2, 0, 21000000);
+  hal_clock_init(&cfg, &pll_cfg);
+  systick_init(1000);
+  uart2_init(9600);
+
+  timer_init(TIM2, 500, 32000);
   timer_enable_interrupt(TIM2);
   timer_attach_callback(TIM2, print2);
-  
-  timer_init(TIM3, 0, 21000000);
+
+  timer_init(TIM3, 500, 32000);
   timer_enable_interrupt(TIM3);
   timer_attach_callback(TIM3, print3);
-  
-  timer_init(TIM4, 0, 21000000);
+
+  timer_init(TIM4, 500, 32000);
   timer_enable_interrupt(TIM4);
   timer_attach_callback(TIM4, print4);
-  
-  timer_init(TIM5, 0, 21000000);
+
+  timer_init(TIM5, 500, 32000);
   timer_enable_interrupt(TIM5);
   timer_attach_callback(TIM5, print5);
-  
-  timer_init(TIM9, 0, 21000000);
+
+  timer_init(TIM9, 500, 32000);
   timer_enable_interrupt(TIM9);
   timer_attach_callback(TIM9, print9);
 
   while (1) {
-    // uart2_write("SysTicks: ");
-    // uart2_write(hal_get_tick());
-    // uart2_write(", AHB: ");
-    // uart2_write(hal_clock_get_ahbclk());
-    // uart2_write(", APB1: ");
-    // uart2_write(hal_clock_get_apb1clk());
-    // uart2_write(", APB2: ");
-    // uart2_write(hal_clock_get_apb2clk());
-    // uart2_write(", Millis: ");
-    // uart2_write(timer_get_count(TIM5));
-    // uart2_write("\n\r");
+    uart2_write("SysTicks: ");
+    uart2_write(hal_get_tick());
+    uart2_write(", AHB: ");
+    uart2_write(hal_clock_get_ahbclk());
+    uart2_write(", APB1: ");
+    uart2_write(hal_clock_get_apb1clk());
+    uart2_write(", APB2: ");
+    uart2_write(hal_clock_get_apb2clk());
+    uart2_write("\n");
     delay_ms(1000);
   }
 }
